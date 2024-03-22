aa7175b152ea05d50d5743a7af11af921e6dc4a0
Ram Sevak Mishra <ramsevakmishra69@gmail.com>
2024-03-22 08:25:25 +0530

    File Metadata Microservice

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+'use strict';
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,6 +15,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
+  }
+});
+
+app.get('/hello', function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
 
 
 
